1d53b2fc32b94f9439d2d2aed5b485d4f70b8cf2
muqniansyah <muqniansyah@gmail.com>
2024-01-20 14:39:16 +0700

    clear

---
+++ pcap programming/baris_baru.py
@@ -0,0 +1,6 @@
+print("hello python!")
+# menggunakan print kosong
+print()
+print("alloo, ini muni...")
+# menggunakan escape character dan newline character
+print("haii pagi  \ntapi sekarang siang ")
